a503d556fce0a586a3b2b81da0549a6e2f8f79e3
Ahmad Aiman <aimanzulbahari@gmail.com>
2023-07-05 08:13:20 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,24 @@ locationBtn = inputPart.querySelector('.button'),
 weatherPart = wrapper.querySelector('.weather-part'),
 wIcon = weatherPart.querySelector('img'),
 arrowBack = wrapper.querySelector('.header i');
+
+let api;
+inputField.addEventListener("keyup", e => {
+    if(e.key == "Enter" && inputField.value != "") {
+        RequestApi(inputField.value)
+    }    
+});
+
+//Request The  City API
+function RequestApi (city) {
+    api = `YOUR_API_KEY`;
+    fetchData();
+}
+
+//Request The Position (Latitude And Longtitude)
+function onSuccess (position) {
+    const {latitude, longtitude} = position.coords;
+    api = `YOUR_API_KEY`;
+    fetch();
+}
+
